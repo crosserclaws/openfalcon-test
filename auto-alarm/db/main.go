269f656cae8136a55af3aa140821242a2e2f2621
crosserclaws <crosserclaws@gmail.com>
2016-01-29 10:59:49 +0800

    Refactoring db into 'select before insert' & IoC

--- auto-alarm/db/main.go
+++ auto-alarm/db/main.go
@@ -12,26 +12,29 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var (
-	dbAddr   string = "root:password@tcp(10.20.30.40:3306)/"
-	dbConfig string = "?charset=utf8&loc=Asia%2FTaipei"
+const (
+	Empty int64 = -1
+)
 
+var (
+	DbAddr   string = "root:password@tcp(10.20.30.40:3306)/"
+	DbConfig string = "?charset=utf8&loc=Asia%2FTaipei"
+
+	Debug      bool
 	testIdBase int    = 7777
 	testIdLast int    = testIdBase + int('z')
 	testAgent  string = "test-agent"
 	timeStamp  string = time.Now().Format("2006-01-02 15:04:05")
 	userList          = []User{
-		{1, "root", "070424e5398ec581c27100a0d63fc86e", "", 2},
-		{testIdBase, "cheminlin", "a5b8fedaf75e5396b3ae2c6e60b55da6", "cheminlin@cepave.com", 2},
+		{"fakeuser1", "070424e5398ec581c27100a0d63fc86e", ""},
+		{"fakeuser2", "070424e5398ec581c27100a0d63fc86e", "cheminlin@cepave.com"},
 	}
 )
 
 type User struct {
-	Id     int
 	Name   string
 	Passwd string
 	Email  string
-	Role   int
 }
 
 type DbCase struct {
@@ -44,6 +47,7 @@ type FnVar func(*sql.DB) []error
 func main() {
 	// Parse cmd-line flags
 	var mode string
+	flag.BoolVar(&Debug, "debug", false, "Debugging or not.")
 	flag.StringVar(&mode, "mode", "",
 		"[cab | clean | build]\n\t"+
 			"build  - Build data. (DB must be clean before build if DB is not empty.)\n\t"+
@@ -67,14 +71,205 @@ func main() {
 }
 
 func build() {
-	var dbSuite = []DbCase{
-		{"falcon_portal", []FnVar{initTbAction, initTbHost, initTbTpl, initTbStrategy, initTbGrp, initTbGrpHost, initTbGrpTpl}},
-		{"uic", []FnVar{initTbUser, initTbTeam, initTbRelTeamUser}},
-	}
 
-	for _, dc := range dbSuite {
-		runDbFlow(dc)
+	buildUic()
+	buildPortal()
+
+}
+
+// IoC Callback for id
+// This method would use sql.DB.QueryRow method to retrive data
+func QueryForId(
+	db *sql.DB,
+	callback func(db *sql.DB) int64,
+	sqlQuery string, args ...interface{},
+) int64 {
+	var id int64
+	fnName := runtime.FuncForPC(reflect.ValueOf(callback).Pointer()).Name()
+	row := db.QueryRow(
+		sqlQuery, args...,
+	)
+
+	err := row.Scan(&id)
+	switch {
+	case err == sql.ErrNoRows:
+		id = callback(db)
+		printDebug(3, "INSERT_ID =", id)
+		if id == Empty {
+			PrintMsg(2, "FAIL!", fnName)
+		} else {
+			PrintMsg(2, "PASS.", fnName)
+		}
+	case err != nil:
+		PrintCheckErr(4, err)
+		PrintMsg(2, "FAIL!", fnName)
+	default:
+		printDebug(3, "SELECT_ID =", id)
+		PrintMsg(2, "PASS.", fnName)
 	}
+	return id
+}
+
+func InsertExec(db *sql.DB, sqlQuery string, args ...interface{}) int64 {
+	return HandleRes(db.Exec(sqlQuery, args...))
+}
+
+func DbConnect(dbName string) *sql.DB {
+	dbSrc := DbAddr + dbName + DbConfig
+	db, err := sql.Open("mysql", dbSrc)
+	if err != nil {
+		log.Fatalf("[Src=%s] %v\n", dbSrc, err)
+	}
+	return db
+}
+
+func PrintCheckErr(skip int, err error) {
+	if err != nil {
+		PrintErr(skip, err)
+	}
+}
+
+func PrintErr(skip int, err error) {
+	PrintMsg(skip, "ERROR", err)
+}
+
+func printDebug(skip int, args ...interface{}) {
+	if Debug {
+		PrintMsg(skip, "DEBUG", args...)
+	}
+}
+
+func PrintMsg(skip int, prefix string, args ...interface{}) {
+	pc, _, line, _ := runtime.Caller(skip)
+	fmt.Printf("[%s][%s:%v] ", prefix, runtime.FuncForPC(pc).Name(), line)
+	fmt.Println(args...)
+}
+
+func HandleRes(res sql.Result, err error) int64 {
+	if err != nil {
+		PrintErr(4, err)
+	} else {
+		id, err := res.LastInsertId()
+		if err != nil {
+			PrintErr(4, err)
+		} else {
+			return id
+		}
+	}
+	return Empty
+}
+
+var (
+	uids []int64
+	uid  int64
+	tid  int64
+	tuid int64
+
+	aid  int64
+	hid  int64
+	gid  int64
+	ghid int64
+	gtid int64
+	tpid int64
+	sid  int64
+)
+
+func buildUic() {
+	db := DbConnect("uic")
+	defer db.Close()
+
+	u := userList[0]
+	uid = QueryForId(db, insertUser,
+		"SELECT id FROM user WHERE name=?", u.Name)
+
+	tid = QueryForId(db, insertTeam,
+		"SELECT id from team WHERE name=?",
+		"fake_user_group")
+
+	tuid = QueryForId(db, insertRelTeamUser,
+		"SELECT id from rel_team_user WHERE tid=? AND uid=?",
+		tid, uid)
+}
+
+func insertUser(db *sql.DB) int64 {
+	u := userList[0]
+	return InsertExec(db,
+		`INSERT INTO user(name ,passwd, email, role, creator, created)
+		VALUES (?, ?, ?, 2, 0, "2016-01-01 01:01:01")`, u.Name, u.Passwd, u.Email)
+}
+
+func insertTeam(db *sql.DB) int64 {
+	return InsertExec(db,
+		`INSERT INTO team(name, resume, creator, created) 
+		VALUES ("fake_user_group", "", ?, "2016-01-01 01:01:01")`, uid)
+}
+
+func insertRelTeamUser(db *sql.DB) int64 {
+	return InsertExec(db,
+		`INSERT INTO rel_team_user (tid, uid)
+		VALUES (?, ?)`, tid, uid)
+}
+
+func buildPortal() {
+	db := DbConnect("falcon_portal")
+	defer db.Close()
+
+	aid = QueryForId(db, insertAction, "SELECT id FROM action WHERE uic=?", "fake_user_group")
+	hid = QueryForId(db, insertHost, "SELECT id FROM  host WHERE hostname=?", testAgent)
+	gid = QueryForId(db, insertGrp, "SELECT id FROM grp WHERE grp_name=?", "fake_host_group")
+	ghid = QueryForId(db, insertGrpHost, "SELECT grp_id FROM grp_host WHERE grp_id=? AND host_id=?", gid, hid)
+	tpid = QueryForId(db, insertTpl, "SELECT id FROM tpl WHERE tpl_name=?", "fake_template")
+	gtid = QueryForId(db, insertGrpTpl, "SELECT grp_id FROM grp_tpl WHERE grp_id=? AND tpl_id=?", gid, tpid)
+	sid = QueryForId(db, insertStrategy, "SELECT id FROM strategy WHERE tpl_id=?", tpid)
+
+}
+
+func insertAction(db *sql.DB) int64 {
+	return InsertExec(db,
+		`INSERT INTO action (uic, url, callback, before_callback_sms, before_callback_mail, after_callback_sms, after_callback_mail) 
+		VALUES ("fake_user_group", "", 0, 0, 0, 0, 0)`)
+}
+
+func insertHost(db *sql.DB) int64 {
+	return InsertExec(db,
+		`INSERT INTO host (hostname, ip, agent_version, plugin_version, maintain_begin, maintain_end, update_at)
+		VALUES (?, '10.20.30.40', '0.0.1', 'plugin not enabled', 0, 0, "2016-01-01 01:01:01")
+		`, testAgent)
+}
+
+func insertGrp(db *sql.DB) int64 {
+	u := userList[0]
+	return InsertExec(db,
+		`INSERT INTO grp (grp_name, create_user, create_at, come_from)
+		VALUES ("fake_host_group", ?, "2016-01-01 01:01:01", 1)`, u.Name)
+}
+
+func insertGrpHost(db *sql.DB) int64 {
+	return InsertExec(db,
+		`INSERT INTO grp_host (grp_id, host_id)
+		VALUES (?, ?)`, gid, hid)
+}
+
+func insertTpl(db *sql.DB) int64 {
+	u := userList[0]
+	return InsertExec(db,
+		`INSERT INTO tpl (tpl_name, parent_id, action_id, create_user, create_at)
+		VALUES ("fake_template", 0, ?, ?, "2016-01-01 01:01:01")`, aid, u.Name)
+}
+
+func insertGrpTpl(db *sql.DB) int64 {
+	u := userList[0]
+	return InsertExec(db,
+		`INSERT INTO grp_tpl (grp_id, tpl_id, bind_user)
+		VALUES (?, ?, ?)
+		`, gid, tpid, u.Name)
+}
+
+func insertStrategy(db *sql.DB) int64 {
+	return InsertExec(db,
+		`INSERT INTO strategy (metric, tags, max_step, priority, func, op, right_value, note, run_begin, run_end, tpl_id)
+		VALUES ("cpu.idle", "", 3, 0, "all(#2)", "<=", 100, "", "", "", ?)
+		`, tpid)
 }
 
 func clean() {
@@ -84,7 +279,7 @@ func clean() {
 }
 
 func cleanDb(dbName string, colName string, tabNames ...string) {
-	db := dbConnect(dbName)
+	db := DbConnect(dbName)
 	defer db.Close()
 
 	for _, tab := range tabNames {
@@ -107,133 +302,6 @@ func cleanTabById(db *sql.DB, tabName string, colName string) []error {
 	stmt := fmt.Sprintf("DELETE FROM %s WHERE %s=?", tabName, colName)
 	for i := testIdBase; i <= testIdLast; i++ {
 		_, err := db.Exec(stmt, i)
-		errList = append(errList, err)
-	}
-	return errList
-}
-
-func runDbFlow(dc DbCase) {
-	db := dbConnect(dc.DbName)
-	defer db.Close()
-
-	for _, ff := range dc.FnFlow {
-		fnName := runtime.FuncForPC(reflect.ValueOf(ff).Pointer()).Name()
-		passFlag := true
-		errList := ff(db)
-		for _, err := range errList {
-			if err != nil {
-				fmt.Printf("[Db=%s][Fn=%s] %v\n", dc.DbName, fnName, err)
-				passFlag = false
-			}
-		}
-		if passFlag {
-			fmt.Printf("[Db=%s][Fn=%s] Pass.\n", dc.DbName, fnName)
-		}
-	}
-}
-
-func dbConnect(dbName string) *sql.DB {
-	dbSrc := dbAddr + dbName + dbConfig
-	db, err := sql.Open("mysql", dbSrc)
-	if err != nil {
-		log.Fatalf("[Src=%s] %v\n", dbSrc, err)
-	}
-	return db
-}
-
-func initTbAction(db *sql.DB) []error {
-	_, err := db.Exec(
-		`
-		INSERT INTO action (id, uic, url, callback, before_callback_sms, before_callback_mail, after_callback_sms, after_callback_mail) 
-		VALUES (?, 'test_user_group', '', 0, 0, 0, 0, 0)
-		`, testIdBase+int('a'))
-	return []error{err}
-}
-
-func initTbHost(db *sql.DB) []error {
-	_, err := db.Exec(
-		`
-		INSERT INTO host (id, hostname, ip, agent_version, plugin_version, maintain_begin, maintain_end, update_at)
-		VALUES (?, ?, '10.20.30.40', '0.0.1', 'plugin not enabled', 0, 0, ?)
-		`, testIdBase+int('h'), testAgent, timeStamp)
-	return []error{err}
-}
-
-func initTbGrp(db *sql.DB) []error {
-	_, err := db.Exec(
-		`
-		INSERT INTO grp (id, grp_name, create_user, create_at, come_from)
-		VALUES (?, 'test_host_group', 'root', ?, 1)
-		`, testIdBase+int('g'), timeStamp)
-	return []error{err}
-}
-
-func initTbGrpHost(db *sql.DB) []error {
-	_, err := db.Exec(
-		`
-		INSERT INTO grp_host (grp_id, host_id)
-		VALUES (?, ?)
-		`, testIdBase+int('g'), testIdBase+int('h'))
-	return []error{err}
-}
-
-func initTbGrpTpl(db *sql.DB) []error {
-	_, err := db.Exec(
-		`
-		INSERT INTO grp_tpl (grp_id, tpl_id, bind_user)
-		VALUES (?, ?, 'root')
-		`, testIdBase+int('g'), testIdBase+int('t'))
-	return []error{err}
-}
-
-func initTbTpl(db *sql.DB) []error {
-	_, err := db.Exec(
-		`
-		INSERT INTO tpl (id, tpl_name, parent_id, action_id, create_user, create_at)
-		VALUES (?, 'test_template', 0, ?, 'root', ?)
-		`, testIdBase+int('t'), testIdBase+int('a'), timeStamp)
-	return []error{err}
-}
-
-func initTbStrategy(db *sql.DB) []error {
-	_, err := db.Exec(
-		`
-		INSERT INTO strategy (id, metric, tags, max_step, priority, func, op, right_value, note, run_begin, run_end, tpl_id)
-		VALUES (?, 'cpu.idle', '', 3, 0, 'all(#2)', '<=', 100, '', '', '', ?)
-		`, testIdBase+int('s'), testIdBase+int('t'))
-	return []error{err}
-}
-
-func initTbRelTeamUser(db *sql.DB) []error {
-	errList := []error{}
-	for k, v := range userList {
-		_, err := db.Exec(
-			`
-			INSERT INTO rel_team_user (id, tid, uid)
-			VALUES (?, ?, ?)
-			`, testIdBase+k, testIdBase, v.Id)
-		errList = append(errList, err)
-	}
-	return errList
-}
-
-func initTbTeam(db *sql.DB) []error {
-	_, err := db.Exec(
-		`
-		INSERT INTO team(id, name, resume, creator, created)
-		VALUES (?, "test_user_group", "", 1, ?)
-		`, testIdBase, timeStamp)
-	return []error{err}
-}
-
-func initTbUser(db *sql.DB) []error {
-	errList := []error{}
-	for _, v := range userList {
-		_, err := db.Exec(
-			`
-				INSERT INTO user(id, name ,passwd, email, role, creator, created) 
-				VALUES (?, ?, ?, ?, ?, 0, ?)
-				`, v.Id, v.Name, v.Passwd, v.Email, v.Role, timeStamp)
 		errList = append(errList, err)
 	}
 	return errList
