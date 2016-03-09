2601ad1d9fc2b1b1139e36f310a2df56543724ee
crosserclaws <crosserclaws@gmail.com>
2016-03-09 14:14:16 +0800

    Apply test framework to suite alarm.

--- pyfalcon/alarm/alarm_00.py
+++ pyfalcon/alarm/alarm_00.py
@@ -1,78 +1,46 @@
 #!/usr/bin/env python3
 
-import sys
-import json
-import argparse
 import requests
 from pyutil import common
 
-_suiteName = 'alarm_00'
-_suiteDesc = 'The API of ALARM/event.'
+_SUITE_NAME = 'alarm_00'
+_SUITE_DESC = 'The API of ALARM/event.'
 
-def init(loggerName, cfgFileName, suiteFileName):
-    # Args
-    parser = argparse.ArgumentParser()
-    common.setParser(parser)
-    args = parser.parse_args()
-    # Logger
-    logger = common.newLogger(loggerName)
-    logger.setLevel(args.loglevel)
-    # Files
-    cfg = common.loadJson(logger, cfgFileName)
-    suite = common.loadJson(logger, suiteFileName)
+def _checkLogin(logger, cfg):
+    if cfg['debug']: return None
     
-    return logger, cfg, suite
-
-def testGetEvent(logger, cfg, param, expect):
-    # Arrange
     loginUrl = cfg['login']['url']
     loginParam = cfg['login']['auth']
     r = requests.post(loginUrl, data=loginParam)
     sig = r.cookies['sig']
     logger.debug("[SIG.] %s", sig)
     
-    # Act
-    testUrl = cfg['host'] + ':' + str(cfg['port']) + cfg['api']
+    return sig
+
+def testGetEvent(logger, tCase, cfg):
+    # Arrange
+    sig = _checkLogin(logger, cfg)
+    testUrl = "http://{:s}:{:d}{:s}".format(cfg['host'], cfg['http'], cfg['api'])
     testCookie = dict(sig=sig)
+    # Act
     r = requests.get(testUrl, cookies=testCookie)
     
     # Assert
-    expectData = expect['counter']
+    expect = tCase['expect']['counter']
     eventList = r.json()
+    logger.debug("[RES<-]\n%s", eventList)
     for event in eventList:
-        realData = event['counter']
-        logger.debug("[DATA.] %s ?= %s", realData, expectData)
-        if realData == expectData:
+        real = event['counter']
+        logger.debug("[DATA.] %s ?= %s", expect, real)
+        if expect == real:
             return True
-    logger.debug("[DATA.] All data != %s", expectData)
+    # logger.debug("[DATA.] All data != %s", expect)
     return False
 
 def main():
-    logger, cfg, suite = init(_suiteName, common.gCfgName, _suiteName + '.json')
-    allPass = True
-
-    # Test
-    for idx, tCase in enumerate(suite):
-        logger.info("[%s][#%02d] testing...", _suiteName ,idx)
-        onePass = testGetEvent(logger, cfg, tCase['data'], tCase['expect'])
-        oneMsg = "[{:s}][#{:02d}] ".format(_suiteName, idx)
-        # Case report
-        if onePass:
-            oneMsg += "PASS."
-        else:
-            allPass = False
-            oneMsg += "FAIL!"
-        print(oneMsg)
-            
-
-    # Suite report
-    allMsg = "[{:s}][ALL] ".format(_suiteName)
-    if allPass:
-        allMsg += "PASS."
-    else:
-        allMsg += "FAIL!"
-    print(allMsg)
-    
+    logger, cfg, suite, _ = common.init(_SUITE_NAME, common.CFG_NAME, _SUITE_NAME + '.json')
+    common.runTestSuite(_SUITE_NAME, testGetEvent, logger, suite, cfg)
 
 if __name__ == "__main__":
+    import sys
     sys.exit(int(main() or 0))
