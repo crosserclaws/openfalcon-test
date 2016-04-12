8c7418ee89987fe50a9d820c523ebb7fcb461fe6
crosserclaws <crosserclaws@gmail.com>
2016-04-12 16:00:20 +0800

    Add test case: graph/index/updateAll. Modify docstrings.

--- pyfalcon/graph/test_graph_005.py
+++ pyfalcon/graph/test_graph_005.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
-""" Functional test of HTTP: graph/api/recv. """
+""" Functional test of HTTP: graph/index/updateAll. """
 
 import pytest
+from pyutil import pytool
 from pyutil.pyhttp import PyHttp
 
 @pytest.mark.parametrize("tCase", [
     {
         "number": "00",
-        "data": [
-            "test_apiRecv_host", "cpu.user", 1456286162, 60, "GAUGE", 0.3579
-        ],
+        "data": {
+        },
         "expect": {
             "msg": "success",
             "data": "ok"
         }
     }
 ])
-def test_apiRecv(gCfg, graphCfg, host, logger, tCase):
+def test_indexUpdateAll(gCfg, graphCfg, host, logger, tCase):
     """
-    Functional test of HTTP: graph/api/recv.
+    Functional test of HTTP: graph/index/updateAll.
     The function sends a GET request; then tests resp `dict`.
     
     :param dict gCfg: Global config in json.
@@ -26,20 +26,12 @@ def test_apiRecv(gCfg, graphCfg, host, logger, tCase):
     :param str host: Host IP to send the request.
     :param logging.Logger logger: A logger named in the module's name.
     :param dict tCase: A test case in json.
-    
-    .. note:: Make sure that the data should in the same order as
-              **endpoint/metric/ts/step/dstype/value** whose types are str/str/int/int/str/float.
-              
-              Here is an URL example: HOST_ADDR/api/recv/host1/cpu.user/1456286162/60/GAUGE/0.3579.
     """
     
-    # Special URL case
-    kwargs = graphCfg['httpApi']['apiRecv']
+    kwargs = graphCfg['httpApi']['indexUpdateAll']
     httpClient = PyHttp(host, graphCfg['http'], logger)
-    data = tCase['data']
-    kwargs['api'] = kwargs['api'] + '/' + '/'.join(map(str, data))
     r = httpClient.call(**kwargs)
     
     expt = tCase['expect']
-    real = r.json() 
+    real = r.json()
     assert expt == real
